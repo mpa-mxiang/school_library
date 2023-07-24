8530b74e27b79a3e7ed4958c3e5f05158551d868
Jiachun Xiang <18576811+mpa-mxiang@users.noreply.github.com>
2023-07-24 16:01:00 -0400

    autocorrect linters errors

--- person.rb
+++ person.rb
@@ -1,31 +1,31 @@
 require_relative 'nameable'
 
 class Person < nameable
-    attr_accessor :name, :age
-    attr_reader :id
-  
-    def initialize(age, parent_permission: true, name: 'Unknown')
-      @id = generate_id
-      @name = name
-      @age = age
-      @parent_permission = parent_permission
-    end
-  
-    def can_use_services?
-      of_age? || @parent_permission
-    end
-  
-    private
-  
-    def of_age?
-      @age >= 18
-    end
-  
-    def generate_id
-      rand(1000..9999)
-    end
-    
-    def correct_name
-        @name
-      end
+  attr_accessor :name, :age
+  attr_reader :id
+
+  def initialize(age, parent_permission: true, name: 'Unknown')
+    @id = generate_id
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
   end
+
+  def can_use_services?
+    of_age? || @parent_permission
+  end
+
+  private
+
+  def of_age?
+    @age >= 18
+  end
+
+  def generate_id
+    rand(1000..9999)
+  end
+
+  def correct_name
+    @name
+  end
+end
