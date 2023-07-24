bc9aca8a91e2eefcebe5683a22940148921b5cf6
Jiachun Xiang <18576811+mpa-mxiang@users.noreply.github.com>
2023-07-24 15:59:50 -0400

    Create main.rb

---
+++ main.rb
@@ -0,0 +1,35 @@
+# main.rb
+require_relative 'person'
+require_relative 'student'
+require_relative 'teacher'
+
+# Create instances of Person, Student, and Teacher
+person = Person.new(25)
+student = Student.new(16, 'Math')
+teacher = Teacher.new(35, 'Physics', 'Mr. Smith')
+
+# Output some information
+puts 'Person Info:'
+puts "ID: #{person.id}, Name: #{person.name}, Age: #{person.age}"
+puts "Can use services? #{person.can_use_services?}"
+
+puts "\nStudent Info:"
+puts "ID: #{student.id}, Name: #{student.name}, Age: #{student.age}, Classroom: #{student.classroom}"
+puts "Can use services? #{student.can_use_services?}"
+puts "Playing hooky: #{student.play_hooky}"
+
+puts "\nTeacher Info:"
+puts "ID: #{teacher.id}, Name: #{teacher.name}, Age: #{teacher.age}, Specialization: #{teacher.specialization}"
+puts "Can use services? #{teacher.can_use_services?}"
+
+person = Person.new(22, name: 'maximilianus')
+puts person.correct_name
+# Output: maximilianus
+
+capitalized_person = CapitalizeDecorator.new(person)
+puts capitalized_person.correct_name
+# Output: Maximilianus
+
+capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
+puts capitalized_trimmed_person.correct_name
+# Output: Maximilian
