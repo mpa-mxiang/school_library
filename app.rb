d2a8087aecb05755d8ea7a966c12e31cd67906b2
Jiachun Xiang <18576811+mpa-mxiang@users.noreply.github.com>
2023-08-01 16:36:09 -0400

    add functions for each option

--- app.rb
+++ app.rb
@@ -50,8 +50,70 @@ class App
     end
   end
 
-  # Implement the methods for each option here.
-  # For example: list_books, list_people, create_teacher, create_student, create_book, create_rental, etc.
+  def list_books
+    @library.books.each do |book|
+      puts "#{book.title} by #{book.author}"
+    end
+  end
+
+  def list_people
+    @library.people.each do |person|
+      puts "#{person.name} (#{person.class})"
+    end
+  end
+
+  def create_teacher
+    print "Enter teacher's name: "
+    name = gets.chomp
+    teacher = Teacher.new(name)
+    @library.add_person(teacher)
+    puts "Teacher #{name} created successfully."
+  end
+
+  def create_student
+    print "Enter student's name: "
+    name = gets.chomp
+    student = Student.new(name)
+    @library.add_person(student)
+    puts "Student #{name} created successfully."
+  end
+
+  def create_book
+    print "Enter book title: "
+    title = gets.chomp
+    print "Enter author name: "
+    author = gets.chomp
+    book = Book.new(title, author)
+    @library.add_book(book)
+    puts "Book '#{title}' by #{author} added to the library."
+  end
+
+  def create_rental
+    puts "Enter student ID:"
+    student_id = gets.chomp.to_i
+    student = @library.find_person_by_id(student_id)
+
+    if student.nil?
+      puts "Student with ID #{student_id} not found."
+    else
+      puts "Available books:"
+      @library.available_books.each_with_index do |book, index|
+        puts "#{index + 1}. #{book.title} by #{book.author}"
+      end
+
+      print "Enter book number to rent: "
+      book_number = gets.chomp.to_i
+
+      if book_number >= 1 && book_number <= @library.available_books.length
+        book = @library.available_books[book_number - 1]
+        rental = Rental.new(student, book)
+        @library.add_rental(rental)
+        puts "#{book.title} rented by #{student.name}."
+      else
+        puts "Invalid book number. Rental failed."
+      end
+    end
+  end
 end
 
 app = App.new
