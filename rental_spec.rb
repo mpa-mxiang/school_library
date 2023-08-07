b6ed936d50baed5f3e462cc2c18b42d93a698f5f
Jiachun Xiang <18576811+mpa-mxiang@users.noreply.github.com>
2023-08-07 17:20:58 -0400

    Create rental_spec.rb

---
+++ rental_spec.rb
@@ -0,0 +1,23 @@
+require_relative 'rental'
+require_relative 'person'
+require_relative 'book'
+
+RSpec.describe Rental do
+  let(:person) { Person.new(25, name: 'John Doe') }
+  let(:book) { Book.new('Title', 'Author') }
+  let(:rental_date) { '2023-07-20' }
+  let(:rental) { Rental.new(rental_date, book, person) }
+
+  describe '#initialize' do
+    it 'creates a new rental with the given date, book, and person' do
+      expect(rental.date).to eq('2023-07-20')
+      expect(rental.book).to eq(book)
+      expect(rental.person).to eq(person)
+    end
+
+    it 'adds the rental to the book and person' do
+      expect(book.rentals).to include(rental)
+      expect(person.rentals).to include(rental)
+    end
+  end
+end
