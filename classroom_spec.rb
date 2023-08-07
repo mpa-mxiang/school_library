10f7121c77506d61e3c5b9b104bf6712b30236cb
Jiachun Xiang <18576811+mpa-mxiang@users.noreply.github.com>
2023-08-07 17:20:56 -0400

    Create classroom_spec.rb

---
+++ classroom_spec.rb
@@ -0,0 +1,25 @@
+require_relative 'classroom'
+require_relative 'student'
+
+RSpec.describe Classroom do
+  let(:classroom) { Classroom.new('Math Class') }
+  let(:student) { Student.new(16, classroom, name: 'Jane Doe') }
+
+  describe '#initialize' do
+    it 'creates a new classroom with the given label' do
+      expect(classroom.label).to eq('Math Class')
+    end
+
+    it 'initializes students as an empty array' do
+      expect(classroom.students).to eq([])
+    end
+  end
+
+  describe '#add_student' do
+    it 'adds a student to the classroom' do
+      classroom.add_student(student)
+      expect(classroom.students).to include(student)
+      expect(student.classroom).to eq(classroom)
+    end
+  end
+end
